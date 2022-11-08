67fbf2cc032e8390ce0f4dbd9e6ff5933ce8dc87
JIAQI ZOU <caishen97888@gmail.com>
2022-11-08 18:07:43 +0900

    eject

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+/* 
+    root component
+*/
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Admin from "./pages/Admin";
+
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch> 
+          <Route path="/login" component={Login}></Route>
+          <Route path="/" exact component={Admin}></Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+/*
+    login menu
+*/
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div>Login</div>
+    )
+  }
+}
